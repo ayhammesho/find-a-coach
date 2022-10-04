bee2353a53229b3117061c1bc32f7697ca844c68
ayhmmesho <ayhammesho@gmail.com>
2022-10-04 19:11:15 +0300

    update the branch
    to deploy it in github pages

--- src/store/modules/requests/getters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  request(state, _, _2, rootGetters) {
-    const coachId = rootGetters.userId;
-    return state.requests.filter((req) => req.coachId === coachId);
-  },
-  hasRequests(_, getters) {
-    return getters.request && getters.request.length > 0;
-  },
-};
